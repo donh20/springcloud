7081801cd98a17984e785105b8c5db4e46fb2e93
Dong Hao <haodong.donh@gmail.com>
2021-02-17 22:23:36 +0800

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	cloud-api-commons/target/cloud-api-commons-1.0-SNAPSHOT.jar
    #	cloud-api-commons/target/maven-status/maven-compiler-plugin/compile/default-compile/inputFiles.lst
    #	cloud-consumer-feign-hystrix-order80/pom.xml
    #	cloud-consumer-feign-hystrix-order80/src/main/java/com/ncamc/springcloud/OrderHystrixMain80.java
    #	cloud-consumer-feign-hystrix-order80/src/main/java/com/ncamc/springcloud/controller/OrderHystrixController.java
    #	cloud-consumer-feign-hystrix-order80/src/main/java/com/ncamc/springcloud/service/PaymentHystrixService.java
    #	cloud-consumer-feign-hystrix-order80/src/main/resources/application.yml
    #	cloud-provider-hystrix-payment8001/pom.xml
    #	cloud-provider-hystrix-payment8001/src/main/java/com/ncamc/springcloud/service/PaymentService.java
    #	cloud-provider-hystrix-payment8001/src/main/resources/application.yml
    #	pom.xml

--- cloud-config-client3366/src/main/java/com/ncamc/springcloud/ConfigClientMain3366.java
+++ cloud-config-client3366/src/main/java/com/ncamc/springcloud/ConfigClientMain3366.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-@EnableEurekaClient
 @SpringBootApplication
-public class ConfigClientMain3355 {
+@EnableEurekaClient
+public class ConfigClientMain3366 {
     public static void main(String[] args) {
-        SpringApplication.run(ConfigClientMain3355.class,args);
+        SpringApplication.run(ConfigClientMain3366.class,args);
     }
 }
