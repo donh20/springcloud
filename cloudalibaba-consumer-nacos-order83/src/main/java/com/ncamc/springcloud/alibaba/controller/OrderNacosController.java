5c86a5d1cbb576c4daeaf0ad9ee71e7e71e5f562
Dong Hao <haodong.donh@gmail.com>
2021-03-30 23:44:37 +0800

    spring cloud alibaba consumer

---
+++ cloudalibaba-consumer-nacos-order83/src/main/java/com/ncamc/springcloud/alibaba/controller/OrderNacosController.java
@@ -0,0 +1,29 @@
+package com.ncamc.springcloud.alibaba.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import javax.annotation.Resource;
+
+@RestController
+@Slf4j
+public class OrderNacosController {
+    @Resource
+    private RestTemplate restTemplate;
+
+    //@Resource
+    //private final String SERVER_URL = "http://nacos-payment-provider";
+    @Value("${service-url.nacos-user-service}")
+    private String serverURL;
+
+    //服务端对外暴露的rest接口 @GetMapping(value = "/payment/nacos/{id}")
+
+    @GetMapping(value = "/consumer/payment/nacos/{id}")
+    public String paymentInfo(@PathVariable("id") Long id) {
+        return restTemplate.getForObject(serverURL+"/payment/nacos/"+id,String.class);
+    }
+}
